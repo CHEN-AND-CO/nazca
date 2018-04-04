540f66676e6b3aad95f294d80debf67b8a576c25
BlivionIaG <kev29lt@gmail.com>
2018-04-04 16:13:54 +0200

    new file:   bdd.sql

---
+++ bdd.sql
@@ -0,0 +1,58 @@
+create database nazca;
+#-------------------------------------------------------------------------------
+#--- Change database -----------------------------------------------------------
+#-------------------------------------------------------------------------------
+use nazca;
+#-------------------------------------------------------------------------------
+#--- Database cleanup ----------------------------------------------------------
+#-------------------------------------------------------------------------------
+drop table if exists parametre;
+drop table if exists cambrure;
+#-------------------------------------------------------------------------------
+#--- Database creation ---------------------------------------------------------
+#-------------------------------------------------------------------------------
+create table parametre
+(
+    id INT not null
+    auto_increment,
+    libelle VARCHAR
+    (40),
+    corde DECIMAL,
+    tmax_p DECIMAL,
+    fmax_p DECIMAL,
+    tmax DECIMAL,
+    fmax DECIMAL,
+    nb_points INT,
+    date DATE,
+    fic_img VARCHAR
+    (256),
+    fic_csv VARCHAR
+    (256),
+    primary key
+    (id)
+)
+engine = innodb;
+    create table cambrure
+    (
+        id int not null
+        auto_increment,
+    x DECIMAL,
+    t DECIMAL,
+    f DECIMAL,
+    yintra DECIMAL,
+    yextra DECIMAL,
+    lgx DECIMAL,
+    primary key
+        (id),
+    foreign key
+        (id_param) references parametre
+        (id)
+)
+engine = innodb;
+
+#-------------------------------------------------------------------------------
+#--- Populate databases --------------------------------------------------------
+#-------------------------------------------------------------------------------
+        set autocommit
+        = 0;
+        set names utf8;
