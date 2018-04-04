7f9a52f37017d7615ab8b9a4d3465fd28ff2b0dc
BlivionIaG <kev29lt@gmail.com>
2018-04-04 16:34:10 +0200

    new file:   php/bdd.php 	new file:   php/constantes.php

---
+++ php/constantes.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @Author: Le Torc'h Kévin
+ * @Company: Chen & Co
+ * @Email: kev29lt@gmail.com
+ * @Created Date: 04-Apr-2018 - 16:28:28
+ * @Last Modified: 04-Apr-2018 - 16:31:45
+ */
+  // Database constants.
+  define('DB_USER', 'user8');
+  define('DB_PASSWORD', 'user9');
+  define('DB_NAME', 'user8');
+  define('DB_SERVER', 'localhost');
+?>
