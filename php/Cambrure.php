24d749d1a037833df538205fc151544752ecb048
BlivionIaG <kev29lt@gmail.com>
2018-04-09 09:13:28 +0200

    modifié :         php/Cambrure.php

--- php/Cambrure.php
+++ php/Cambrure.php
@@ -1,17 +1,80 @@
 <?php
 
-class Cambrure  
-{
-    function __construct($C, $Tmax, $Fmax, $nbPoints)
-    {
-        $this->C = $C;                      //Corde
-        $this->Fmax = $Fmax;                //Fmax
-        $this->Tmax = $Tmax;                //Tmax
-        $this->nbPoints = $nbPoints;        //Nombre de points à calculer
+class Cambrure {
 
-        
+    private $x;
+    private $t;
+    private $f;
+    private $yintra;
+    private $yextra;
+    private $id_param;
+    private $lgx;
+
+    function init($_x, $_t, $_f, $_yintra, $_yextra, $_id_param, $_lgx) {
+        $this->x = $_x;
+        $this->t = $_t;
+        $this->f = $_f;
+        $this->yintra = $_yintra;
+        $this->yextra = $_yextra;
+        $this->id_param = $_id_param;
+        $this->lgx = $_lgx;
     }
 
+    function setX($_x) {
+        $this->x = $_x;
+    }
+
+    function setT($_t) {
+        $this->t = $_t;
+    }
+
+    function setF($_f) {
+        $this->f = $_f;
+    }
+
+    function setYintra($_yintra) {
+        $this->yintra = $_yintra;
+    }
+
+    function setYextra($y_extra) {
+        $this->yextra = $y_extra;
+    }
+
+    function setId_param($_id_param) {
+        $this->id_param = $_id_param;
+    }
+
+    function setLgx($_lgx) {
+        $this->lgx = $_lgx;
+    }
+
+    function getX() {
+        return $this->x;
+    }
+
+    function getT() {
+        return $this->t;
+    }
+
+    function getF() {
+        return $this->f;
+    }
+
+    function getYintra() {
+        return $this->yintra;
+    }
+
+    function getYextra() {
+        return $this->yextra;
+    }
+
+    function getId_param() {
+        return $this->id_param;
+    }
+
+    function getLgx() {
+        return $this->lgx;
+    }
 }
 
 ?>
