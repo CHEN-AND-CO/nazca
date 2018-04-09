1c611938db37dbb395693b51fe536f4f6bc60d13
BlivionIaG <kev29lt@gmail.com>
2018-04-09 10:15:29 +0200

    modifié :         Parametre.php 	modifié :         index.php

--- Parametre.php
+++ Parametre.php
@@ -28,5 +28,32 @@ class Parametre {
     public function setCorde($_corde){
         $this->corde = $_corde;
     }
-
+    
+    public function setTmax_p($_tmax_p){
+        $this->tmax_p = $_tmax_p;
+    }
+    
+    public function setFmax_p($_fmax_p){
+        $this->fmax_p = $_fmax_p;
+    }
+    
+    public function setTmax($_tmax){
+        $this->tmax = $_tmax;
+    }
+    
+    public function setFmax($_fmax){
+        $this->fmax = $_fmax;
+    }
+    
+    public function setNb_points($_nb_points){
+        $this->nb_points = $_nb_points;
+    }
+    
+    public function setFic_img($_fic_img){
+        $this->fic_img = $_fic_img;
+    }
+    
+    public function setFic_csv($_fic_csv){
+        $this->fic_csv = $_fic_csv;
+    }
 }
--- index.php
+++ index.php
@@ -25,7 +25,6 @@
 
         $db = dbConnect();
         
-        
         ?>
 
         <footer class="footer">
