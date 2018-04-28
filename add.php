87543dc71b3ff37e981e3b8f801d2c72ae7a7199
BlivionIaG <kev29lt@gmail.com>
2018-04-28 21:47:25 +0200

    modifié :         add.php

--- add.php
+++ add.php
@@ -13,8 +13,9 @@
         ?>
 
         <div class="main-content">
+            <!--iframe name="hiddenFrame" width="0" height="0" border="0" style="display: none;"></iframe-->
+            <!--form class="vbox" action="action.php" target="hiddenFrame" method="POST"-->
             <form class="vbox" action="action.php" method="POST">
-
                 <h3>Paramétres de profil</h3>
                 <input type="hidden" name="action" value="add_param">
 
@@ -32,7 +33,7 @@
                 </div>
 
                 <input type="number" name="nb_points" id="nb_points" placeholder="Nombre de points de calcul">
-                
+
                 <button type="submit">Enregistrer et créer le profil</button>
             </form>
         </div>
