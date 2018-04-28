6e985e8a5923edf05e308233f05c1b738bade185
BlivionIaG <kev29lt@gmail.com>
2018-04-28 22:04:06 +0200

    modifié :         action.php

--- action.php
+++ action.php
@@ -1,103 +1,100 @@
-<?php
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Création de profil - NAZCA</title>
 
-require_once('php/bdd.php');
+        <link rel="stylesheet" type="text/css" media="screen" href="css/index.css" />
+        <link rel="stylesheet" href="css/generic-theme.css">
+    </head>
 
-$db = new BDDIO;
+    <body id="#page-list">
+        <?php
+        include("res/header.html");
+        ?> 
+        <div class="main-content">
+            <?php
+            require_once('php/bdd.php');
 
-echo "<b>dirty php debugging. if you don't want to loose your soul, get out.</b><br>begin action";
+            $db = new BDDIO;
 
-$action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+            $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
 
-if (isset($action)) {
-    echo "<br>action is set, begin processing";
+            if (isset($action)) {
+                if ($action === 'del_param') {
+                    $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
 
-    if ($action === 'del_param') {
+                    if (!isset($id)) {
+                        // wrong id or not set  
+                    } else if ($db->RemoveParam($id)) {
+                        //success
+                    } else {
+                        //failure
+                    }
+                } else if ($action === 'add_param' || $action === 'update_param') {
+                    if ($action === 'update_param') {
+                        $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+                    }
 
-        echo "<br>delete";
-        $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+                    $libelle = filter_input(INPUT_POST, 'libelle', FILTER_SANITIZE_STRING);
+                    $corde = filter_input(INPUT_POST, 'corde', FILTER_SANITIZE_NUMBER_FLOAT);
+                    $tmax_p = filter_input(INPUT_POST, 'tmax_p', FILTER_SANITIZE_NUMBER_FLOAT);
+                    $fmax_p = filter_input(INPUT_POST, 'fmax_p', FILTER_SANITIZE_NUMBER_FLOAT);
+                    $tmax = filter_input(INPUT_POST, 'tmax', FILTER_SANITIZE_NUMBER_FLOAT);
+                    $fmax = filter_input(INPUT_POST, 'fmax', FILTER_SANITIZE_NUMBER_FLOAT);
+                    $nb_points = filter_input(INPUT_POST, 'nb_points', FILTER_SANITIZE_NUMBER_INT);
+                    $date = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING);
+                    $fic_img = filter_input(INPUT_POST, 'fic_img', FILTER_SANITIZE_URL);
+                    $fic_csv = filter_input(INPUT_POST, 'fic_csv', FILTER_SANITIZE_URL);
 
-        if (!isset($id)) {
-            // wrong id or not set  
-        } else if ($db->RemoveParam($id)) {
-            //success
-        } else {
-            //failure
-        }
-    } else if ($action === 'add_param' || $action === 'update_param') {
+                    if (!(isset($libelle) && isset($corde) && isset($tmax_p) && isset($fmax_p) && isset($tmax) && isset($fmax) && isset($nb_points))) {
+                        echo '<h2> ERREUR: Une ou des valeurs requises pour la création d\'un paramètre est/sont invalide(s) </h2>';
+                    } else {
+                        if (!isset($date)) {
+                            $date = date_create()->format('Y-m-d');
+                        }
+                        if (!isset($fic_csv)) {
+                            $fic_csv = '#';
+                        }
+                        if (!isset($fic_img)) {
+                            $fic_img = '#';
+                        }
 
-        echo "<br>parsing...";
+                        $param = new Parametre;
 
-        if ($action === 'update_param') {
-            $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
-        }
+                        $param->init(0, $libelle, $corde, $tmax_p, $fmax_p, $tmax, $fmax, $nb_points, $date, $fic_img, $fic_csv);
 
-        $libelle = filter_input(INPUT_POST, 'libelle', FILTER_SANITIZE_STRING);
-        $corde = filter_input(INPUT_POST, 'corde', FILTER_SANITIZE_NUMBER_FLOAT);
-        $tmax_p = filter_input(INPUT_POST, 'tmax_p', FILTER_SANITIZE_NUMBER_FLOAT);
-        $fmax_p = filter_input(INPUT_POST, 'fmax_p', FILTER_SANITIZE_NUMBER_FLOAT);
-        $tmax = filter_input(INPUT_POST, 'tmax', FILTER_SANITIZE_NUMBER_FLOAT);
-        $fmax = filter_input(INPUT_POST, 'fmax', FILTER_SANITIZE_NUMBER_FLOAT);
-        $nb_points = filter_input(INPUT_POST, 'nb_points', FILTER_SANITIZE_NUMBER_INT);
-        $date = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING);
-        $fic_img = filter_input(INPUT_POST, 'fic_img', FILTER_SANITIZE_URL);
-        $fic_csv = filter_input(INPUT_POST, 'fic_csv', FILTER_SANITIZE_URL);
-
-        echo "done";
-
-        if (!(isset($libelle) && isset($corde) && isset($tmax_p) && isset($fmax_p) && isset($tmax) && isset($fmax) && isset($nb_points))) {
-
-            echo "<br>ERR_ACTION_INVALIDPARAM";
-        } else {
-
-            echo "<br>committing.";
-
-            if (!isset($date)) {
-                $date = date_create()->format('Y-m-d');
-            }
-            echo ".";
-            if (!isset($fic_csv)) {
-                $fic_csv = '#';
-            }
-            echo ".";
-            if (!isset($fic_img)) {
-                $fic_img = '#';
-            }
-
-            echo ".>";
-
-            $param = new Parametre;
-
-            $param->init(0, $libelle, $corde, $tmax_p, $fmax_p, $tmax, $fmax, $nb_points, $date, $fic_img, $fic_csv);
-            echo ">";
-
-            echo "This will never appear";
-
-            if ($action === 'add_param') {
-                if ($db->AddParamObject($param)) {
-                    echo "<br>ERR_ACTION_ADD_NOERROR";
+                        if ($action === 'add_param') {
+                            if ($db->AddParamObject($param)) {
+                                echo '<h2> Vous avez rajouté ' . $param->getLibelle() . ' à la Base de donnée !</h2>';
+                            } else {
+                                echo '<h2> ERREUR: Impossible d\'ajouter ' . $param->getLibelle() . ' à la Base de donnée !</h2>';
+                            }
+                        } else {
+                            if ($db->UpdateParam($id, $param)) {
+                                echo '<h2> Vous avez modifié ' . $param->getLibelle() . ' avec succès ! </h2>';
+                            } else {
+                                echo '<h2> ERREUR: Impossible de modifier ' . $param->getLibelle() . ' dans la Base de donnée !</h2>';
+                            }
+                        }
+                    }
+                } else if ($action === 'del_cambrure') {
+                    echo "<br>ERR_ACTION_NOTIMPL";
+                } else if ($action === 'add_cambrure') {
+                    echo "<br>ERR_ACTION_NOTIMPL";
                 } else {
-                    echo "<br>ERR_ACTION_ADD_FAILED";
+                    echo '<h2> ERREUR: Cette action n\'existe pas </h2>';
                 }
             } else {
-                if ($db->UpdateParam($id, $param)) {
-                    echo "<br>ERR_ACTION_UPDATE_NOERROR";
-                } else {
-                    echo "<br>ERR_ACTION_UPDATE_FAILED";
-                }
+                echo "<br>ERR_ACTION_UNSET";
             }
 
-            echo "done";
-        }
-    } else if ($action === 'del_cambrure') {
-        echo "<br>ERR_ACTION_NOTIMPL";
-    } else if ($action === 'add_cambrure') {
-        echo "<br>ERR_ACTION_NOTIMPL";
-    } else {
-        echo "<br>ERR_ACTION_UNKNOWNACTION";
-    }
-} else {
-    echo "<br>ERR_ACTION_UNSET";
-}
+            echo '<a class="button" href="/">Retour à l\'acceuil</a>';
+            echo '<a class="button" href="/ add.php">Recommencer</a>';
+            ?>
+        </div>
+        <?php
+        include("res/footer.html");
+        ?>
 
-echo "<br>end";
-?>
+    </body>
+</html>
