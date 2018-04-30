14a80a231ffa945abea5b121eb8ad89b8daf24f8
HerrCraziDev <herrcrazi@gmail.com>
2018-04-30 18:53:43 +0200

    [Blitzkrieg] Updated by Blitzkrieg v0.0.2

--- consultation.php
+++ consultation.php
@@ -18,8 +18,6 @@
             <div id="infos" class="container">
             </div>
 
-            <h2>Consultation du paramètre</h2>
-
             <?php
             require_once('php/bdd.php');
 
@@ -32,51 +30,74 @@
 
                 $db = new BDDIO;
 
-                $param = $db->RequestParam($id);
+                $param = $db->RequestParam($id)[0];
 
-                if ($param) echo "teeeeeeeest";
-                
-                $cambrures = $db->RequestAllCambruresFromParam(intval($_GET['id']));
+                if ($param) 
+                {
+                    echo "<h2>Profil <i>".$param->getLibelle()."</i></h2>";
 
-                echo '<table class="flat-table"><thead><td>id</td><td>x</td><td>f</td><td>t</td><td>y<sub>intra</sub></td><td>y<sub>extra</sub></td><td>id_param</td><td>Ig<sub>z</sub></td></thead>';
-                foreach ($cambrures as $cambrure) {
-                    echo '<tr>';
-                    echo '<td>' . round( $cambrure->getId(), 2) . '</td>';
-                    echo '<td>' . round( $cambrure->getX(), 2) . '</td>';
-                    echo '<td>' . round( $cambrure->getF(), 2) . '</td>';
-                    echo '<td>' . round( $cambrure->getT(), 2) . '</td>';
-                    echo '<td>' . round( $cambrure->getYintra(), 2) . '</td>';
-                    echo '<td>' . round( $cambrure->getYextra(), 2) . '</td>';
-                    echo '<td>' . round( $cambrure->getId_param(), 2) . '</td>';
-                    echo '<td>' . round( $cambrure->getIgz(), 5) . '</td>';
-                    echo '</tr>';
+                    echo '<ul>';
+                        echo '<li><b>N° : </b>' . $param->getId() . '</li>';
+                        echo '<li><b>Nom : </b>' . $param->getLibelle() . '</li>';
+                        echo '<li><b>Corde : </b>' . $param->getCorde() . ' mm</li>';
+                        echo '<li><b>T<sub>max</sub> : </b>' . $param->getTmax() . 'mm (' . $param->getTmax_p() . ' %)</li>';
+                        echo '<li><b>F<sub>max</sub> : </b>' . $param->getFmax() . 'mm (' . $param->getFmax_p() . ' %)</li>';
+                        echo '<li><b>Nom : </b>' . $param->getLibelle() . '</li>';
+                    echo '</ul>';
+                    
+                    $cambrures = $db->RequestAllCambruresFromParam(intval($_GET['id']));
+
+                    echo '<table class="flat-table">';
+                        echo '<thead>';
+                            echo '<td>id</td>';
+                            echo '<td>x</td>';
+                            echo '<td>f</td>';
+                            echo '<td>t</td>';
+                            echo '<td>y<sub>intra</sub></td>';
+                            echo '<td>y<sub>extra</sub></td>';
+                            echo '<td>id_param</td>';
+                            echo '<td>Ig<sub>z</sub></td>';
+                        echo '</thead>';
+
+                    foreach ($cambrures as $cambrure) {
+                        echo '<tr>';
+                            echo '<td>' . round( $cambrure->getId(), 2) . '</td>';
+                            echo '<td>' . round( $cambrure->getX(), 2) . '</td>';
+                            echo '<td>' . round( $cambrure->getF(), 2) . '</td>';
+                            echo '<td>' . round( $cambrure->getT(), 2) . '</td>';
+                            echo '<td>' . round( $cambrure->getYintra(), 2) . '</td>';
+                            echo '<td>' . round( $cambrure->getYextra(), 2) . '</td>';
+                            echo '<td>' . round( $cambrure->getId_param(), 2) . '</td>';
+                            echo '<td>' . round( $cambrure->getIgz(), 5) . '</td>';
+                        echo '</tr>';
+                    }
+
+                    echo '</table>';
+
+
+                    echo '<br></br><h2> test de chargement de CSV </h2>';
+
+                    require_once('php/CSVIO.php');
+
+                    $cambrures2 = CSVIO::getCSVFileAsCambrureArray('test.csv');
+
+                    echo '<table class="flat-table"><thead><td>id</td><td>x</td><td>f</td><td>t</td><td>yintra</td><td>yextra</td><td>id_param</td><td>Igz</td></thead>';
+                    foreach ($cambrures2 as $cambrure) {
+                        echo '<tr>';
+                        echo '<td>' . $cambrure->getId() . '</td>';
+                        echo '<td>' . $cambrure->getX() . '</td>';
+                        echo '<td>' . $cambrure->getF() . '</td>';
+                        echo '<td>' . $cambrure->getT() . '</td>';
+                        echo '<td>' . $cambrure->getYintra() . '</td>';
+                        echo '<td>' . $cambrure->getYextra() . '</td>';
+                        echo '<td>' . $cambrure->getId_param() . '</td>';
+                        echo '<td>' . $cambrure->getIgz() . '</td>';
+                        echo '</tr>';
+                    }
+                    echo '</table>';
+
+                    CSVIO::writeCambrureArrayToCSVFile('test' . date_create()->format('Y-m-d') . '.csv', $cambrures);
                 }
-                echo '</table>';
-
-
-                echo '<br></br><h2> test de chargement de CSV </h2>';
-
-                require_once('php/CSVIO.php');
-
-                $cambrures2 = CSVIO::getCSVFileAsCambrureArray('test.csv');
-
-                echo '<table class="flat-table"><thead><td>id</td><td>x</td><td>f</td><td>t</td><td>yintra</td><td>yextra</td><td>id_param</td><td>Igz</td></thead>';
-                foreach ($cambrures2 as $cambrure) {
-                    echo '<tr>';
-                    echo '<td>' . $cambrure->getId() . '</td>';
-                    echo '<td>' . $cambrure->getX() . '</td>';
-                    echo '<td>' . $cambrure->getF() . '</td>';
-                    echo '<td>' . $cambrure->getT() . '</td>';
-                    echo '<td>' . $cambrure->getYintra() . '</td>';
-                    echo '<td>' . $cambrure->getYextra() . '</td>';
-                    echo '<td>' . $cambrure->getId_param() . '</td>';
-                    echo '<td>' . $cambrure->getIgz() . '</td>';
-                    echo '</tr>';
-                }
-                echo '</table>';
-
-                CSVIO::writeCambrureArrayToCSVFile('test' . date_create()->format('Y-m-d') . '.csv', $cambrures);
-                
             }
             ?>
 
