579f2b422cf2555a391dafea1a3533c74a843a7a
HerrCraziDev <herrcrazi@gmail.com>
2018-05-02 19:36:36 +0200

    [Blitzkrieg] Updated by Blitzkrieg v0.0.2

--- add.php
+++ add.php
@@ -22,6 +22,14 @@
             <div id="inner-circle"></div>
         </div>
 
+
+        <?php
+        require_once('php/bdd.php');
+        
+        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+        if (!isset($id)) {
+        ?>
         <!-- Formulaire d'ajout/édition de profil-->
         <div class="main-content">
             <form id="edit-param" class="vbox" action="action.php" method="GET">
@@ -41,6 +49,44 @@
                 <button type="submit" id="btn-submit">Enregistrer et créer le profil</button>
             </form>
         </div>
+
+        <?php
+        } else {
+            /* Initialisation de l'interface avec la BDD */
+            $db = new BDDIO;
+
+            /* Récupération du paramètre à consulter */
+            $param = $db->RequestParam($id)[0];
+
+            /* S'il existe/valide */
+            if ($param)
+            {
+        ?>
+
+        <div class="main-content">
+            <form id="edit-param" class="vbox" action="action.php" method="GET">
+                <h3>Paramètres de profil</h3>
+                <input type="hidden" name="action" value="add_param">
+
+                <?php
+                echo '<input type="text" name="libelle" id="libelle" placeholder="Nom de l\'enregistrement" value="'.$param->getLibelle().'">';
+                echo '<input type="number" name="corde" min="0" step="0.001" id="corde" placeholder="Corde" value="'.$param->getCorde().'">';
+
+                echo '<div class="hbox">';
+                    echo '<input type="number" name="tmax_p" id="tmax_p" step="any" min="0" placeholder="T max (pourcentage)" value="'.$param->getTmax().'">';
+                    echo '<input type="number" name="fmax_p" id="fmax_p" step="any" min="0" placeholder="F max (pourcentage)" value="'.$param->getFmax().'">';
+                echo '</div>';
+
+                echo '<input type="number" name="nb_points" id="nb_points" step="1" min="0" max="10000" placeholder="Nombre de points de calcul" value="'.$param->getNb_points().'">';
+                ?>
+                <button type="submit" id="btn-submit">Enregistrer les modifications</button>
+            </form>
+        </div>
+
+        <?php
+            }
+        }
+        ?>
 
         <?php
         include("res/footer.html");
