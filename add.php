8337dd4df0add28635e74f8423840e8d09fd6a79
BlivionIaG <kev29lt@gmail.com>
2018-05-02 19:44:44 +0200

    modifié :         add.php 	OLDREADME.md 	res/csv/ 	res/img/

--- add.php
+++ add.php
@@ -73,8 +73,8 @@
                 echo '<input type="number" name="corde" min="0" step="0.001" id="corde" placeholder="Corde" value="'.$param->getCorde().'">';
 
                 echo '<div class="hbox">';
-                    echo '<input type="number" name="tmax_p" id="tmax_p" step="any" min="0" placeholder="T max (pourcentage)" value="'.$param->getTmax().'">';
-                    echo '<input type="number" name="fmax_p" id="fmax_p" step="any" min="0" placeholder="F max (pourcentage)" value="'.$param->getFmax().'">';
+                    echo '<input type="number" name="tmax_p" id="tmax_p" step="any" min="0" placeholder="T max (pourcentage)" value="'.$param->getTmax_p().'">';
+                    echo '<input type="number" name="fmax_p" id="fmax_p" step="any" min="0" placeholder="F max (pourcentage)" value="'.$param->getFmax_p().'">';
                 echo '</div>';
 
                 echo '<input type="number" name="nb_points" id="nb_points" step="1" min="0" max="10000" placeholder="Nombre de points de calcul" value="'.$param->getNb_points().'">';
